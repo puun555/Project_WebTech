ee78d7c097e59a8399b198840237fc380b145355
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-11-05 13:39:41 +0700

    Mai

--- pepsi/src/App.js
+++ pepsi/src/App.js
@@ -1,14 +1,16 @@
-
+import ProductPage from './components/product/ProductPage'
 import Homepage from './components/HomePage/homePage';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import PurchesPage from './components/purchase/purchase';
+import ProductDetail from './components/product/ProductDetail/ProductDetail';
 function App() {
   return (
-
         <Routes>
-          <Route path="/" element={<Homepage></Homepage>}></Route>
-          <Route path="/purches" element={<PurchesPage></PurchesPage>}></Route>
+          <Route path="/" element={<Homepage/>}></Route>
+          <Route path="/purches" element={<PurchesPage/>}></Route>
+          <Route path="/product" element={<ProductPage/>}></Route>
+          <Route path="/product/:page" element={<ProductDetail/>}></Route>
         </Routes>
   );
 }
