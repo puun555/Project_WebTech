e7018fd98389d92e1fd10e196f7227bca7321c79
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-11-10 13:22:17 +0700

    Edited Product

--- pepsi/src/components/product/ProductPage.js
+++ pepsi/src/components/product/ProductPage.js
@@ -4,6 +4,7 @@ import Navbar from "../HomePage/navbar";
 import PepsiLogo from "./ProductDetail/img_products/Pepsi-Logo.png"
 import FantaLogo from "./ProductDetail/img_products/Fanta-Logo.png"
 import SpriteLogo from "./ProductDetail/img_products/Sprite-Logo.png"
+import SchweppesLogo from "./ProductDetail/img_products/Schweppes-Logo.png"
 import "./ProductPage.css"
 const ProductPage = () =>{
   {window.scroll(0,0)}
@@ -12,39 +13,39 @@ const ProductPage = () =>{
           <Navbar />
           <div className="product-container">
             <div className="product-header">
-              <img src={PepsiLogo} style={{width:"30%"}} alt=""/>
+              <img src={PepsiLogo} style={{width:"20%"}} alt=""/>
             </div>
             <div className="product-body">
-            {products.filter(prod => prod.name.includes("PEPSI")).map(filteredName => (
-              <Product {...filteredName} key={filteredName.id}/>
-            ))}
+              {products.filter(prod => prod.name.includes("PEPSI")).map(filteredName => (
+                <Product {...filteredName} key={filteredName.id}/>
+              ))}
             </div>
 
             <div className="product-header">
               <img src={FantaLogo} style={{width:"30%"}} alt=""/>
             </div>
             <div className="product-body">
-            {products.filter(prod => prod.name.includes("FANTA")).map(filteredName => (
-              <Product {...filteredName} key={filteredName.id}/>
-            ))}
+              {products.filter(prod => prod.name.includes("FANTA")).map(filteredName => (
+                <Product {...filteredName} key={filteredName.id}/>
+              ))}
             </div>
               
             <div className="product-header">
-              <h1>SCHWEPPES</h1>
+            <img src={SchweppesLogo} style={{width:"90%"}} alt=""/>
             </div>
             <div className="product-body">
-            {products.filter(prod => prod.name.includes("SCHWEPPES")).map(filteredName => (
-              <Product {...filteredName} key={filteredName.id}/>
-            ))}
+              {products.filter(prod => prod.name.includes("SCHWEPPES")).map(filteredName => (
+                <Product {...filteredName} key={filteredName.id}/>
+              ))}
             </div>
 
             <div className="product-header">
             <img src={SpriteLogo} style={{width:"30%"}} alt=""/>
             </div>
             <div className="product-body">
-            {products.filter(prod => prod.name.includes("SPRITE")).map(filteredName => (
-              <Product {...filteredName} key={filteredName.id}/>
-            ))}
+              {products.filter(prod => prod.name.includes("SPRITE")).map(filteredName => (
+                <Product {...filteredName} key={filteredName.id}/>
+              ))}
             </div>
           </div>
     </div>
