c2627f590958e00d56306cfdb553481cfcb8aacf
Teerawat Sangjun <88505847+TeerawatSangjun@users.noreply.github.com>
2022-11-13 20:16:52 +0700

    footer hover color

--- pepsi/src/components/HomePage/footer.js
+++ pepsi/src/components/HomePage/footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
             <div className='footer-header'>
                 <h2>KUMI SHOP</h2>
             </div>
-            <div>
+            <div className='hovercolor'>
                 Questions?
             </div>
             <div className='footer-centerbottom'>
-                <div>
+                <div className='hovercolor'>
                     Home
                 </div>
                 <div className='footer-services'>
@@ -21,28 +21,34 @@ const Footer = () => {
             </div>
             <div>
                 <a href=""
-                    className="youtube social">
+                    className="youtube">
                     <img className='icon' src='socials_icon/youtube.png'></img>
+                    <img className='icon-hover' src='socials_icon/youtube_hover.png'></img>
                 </a>
                 <a href=""
-                    className="facebook social">
+                    className="facebook">
                     <img className='icon' src='socials_icon/facebook.png'></img>
+                    <img className='icon-hover' src='socials_icon/facebook_hover.png'></img>
                 </a>
                 <a href="" 
-                    className="twitter social">
+                    className="twitter">
                     <img className='icon' src='socials_icon/twitter.png'></img>
+                    <img className='icon-hover' src='socials_icon/twitter_hover.png'></img>
                 </a>
                 <a href=""
-                    className="instagram social">
+                    className="instagram">
                     <img className='icon' src='socials_icon/instagram.png'></img>
+                    <img className='icon-hover' src='socials_icon/instagram_hover.png'></img>
                 </a>
                 <a href=""
-                    className="discord social">
+                    className="discord">
                     <img className='icon' src='socials_icon/discord.png'></img>
+                    <img className='icon-hover' src='socials_icon/discord_hover.png'></img>
                 </a>
                 <a href=""
-                    className="tiktok social">
+                    className="tiktok">
                     <img className='icon' src='socials_icon/tiktok.png'></img>
+                    <img className='icon-hover' src='socials_icon/tiktok_hover.png'></img>
                 </a>
             </div>
         </div>
