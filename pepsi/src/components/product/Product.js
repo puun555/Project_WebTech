694e322b9104ca9e4fd33d14df858a8a35f2c6d9
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-11-13 18:19:37 +0700

    กำลังแก้ใหม่

--- pepsi/src/components/product/Product.js
+++ pepsi/src/components/product/Product.js
@@ -2,10 +2,18 @@ import {Link} from "react-router-dom";
 import "./ProductPage.css"
 const Product = ({name,img_product,background_color,id,page}) =>{
     return (
+        <div className="product-item-body">
             <div className="product-list">
                 <img src={img_product} alt=""/>
-                <strong>{name}</strong>
             </div>
+            <div className="product-text">
+                <p>{name}</p>
+                
+                <Link to={{
+                    pathname : `/product/${id-1}`,
+                }}>Read More</Link>
+            </div>
+        </div>
     )
 }
 export default Product
--- pepsi/src/components/product/ProductPage.js
+++ pepsi/src/components/product/ProductPage.js
@@ -1,55 +1,44 @@
 import Product from "./Product"
 import products from "./products.json"
 import Navbar from "../HomePage/navbar";
-import PepsiLogo from "./ProductDetail/img_products/Pepsi-Logo.png"
-import FantaLogo from "./ProductDetail/img_products/Fanta-Logo.png"
-import SpriteLogo from "./ProductDetail/img_products/Sprite-Logo.png"
-import SchweppesLogo from "./ProductDetail/img_products/Schweppes-Logo.png"
+import PEPSI from "./ProductDetail/img_products/Pepsi-Logo.png"
+import FANTA from "./ProductDetail/img_products/Fanta-Logo.png"
+import SPRITE from "./ProductDetail/img_products/Sprite-Logo.png"
+import SCHWEPPES from "./ProductDetail/img_products/Schweppes-Logo.png"
 import "./ProductPage.css"
+import { useState } from "react";
 const ProductPage = () =>{
-  {window.scroll(0,0)}
+  const wraperFunction = (name,img) =>{
+    setProd(name)
+    setImg(img);
+  }
+  const [prods,setProd] = useState("PEPSI");
+  const [img,setImg] = useState(PEPSI);
   return (
-    <div style={{background:"#e4e6e7"}}>
+    <div style={{background:"#fff"}}>
           <Navbar />
+          <div className="product-navbar">
+            <h2 onClick={() => wraperFunction("PEPSI",PEPSI)}>PEPSI</h2>
+            <h2 onClick={() => wraperFunction("FANTA",FANTA)}>FANTA</h2>
+            <h2 onClick={() => wraperFunction("SCHWEPPES",SCHWEPPES)}>SCHWEPPES</h2>
+            <h2 onClick={() => wraperFunction("SPRITE",SPRITE)}>SPRITE</h2>
+          </div>
           <div className="product-container">
-            <div className="product-header">
-              <img src={PepsiLogo} style={{width:"20%"}} alt=""/>
+            <div className="product-header" >
+              <img src={img} style={{width:"20%"}} alt=""/>
             </div>
-            <div className="product-body">
-              {products.filter(prod => prod.name.includes("PEPSI")).map(filteredName => (
+            <div className="product-body" id="product-pepsi">
+              {products.filter(prod => prod.name.includes(prods)).map(filteredName => (
                 <Product {...filteredName} key={filteredName.id}/>
               ))}
             </div>
 
-            <div className="product-header">
-              <img src={FantaLogo} style={{width:"30%"}} alt=""/>
-            </div>
-            <div className="product-body">
-              {products.filter(prod => prod.name.includes("FANTA")).map(filteredName => (
-                <Product {...filteredName} key={filteredName.id}/>
-              ))}
-            </div>
-              
-            <div className="product-header">
-            <img src={SchweppesLogo} style={{width:"90%"}} alt=""/>
-            </div>
-            <div className="product-body">
-              {products.filter(prod => prod.name.includes("SCHWEPPES")).map(filteredName => (
-                <Product {...filteredName} key={filteredName.id}/>
-              ))}
-            </div>
-
-            <div className="product-header">
-            <img src={SpriteLogo} style={{width:"30%"}} alt=""/>
-            </div>
-            <div className="product-body">
-              {products.filter(prod => prod.name.includes("SPRITE")).map(filteredName => (
-                <Product {...filteredName} key={filteredName.id}/>
-              ))}
-            </div>
+            
           </div>
     </div>
+    
   )
+  
 }
 
 export default ProductPage;
