32c79b6c598c303b992ba44340c987c8bc8d419f
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-11-14 14:54:43 +0700

    Updated

--- pepsi/src/components/product/ProductDetail/ProductDetail.js
+++ pepsi/src/components/product/ProductDetail/ProductDetail.js
@@ -2,14 +2,25 @@ import Navbar from "../../HomePage/navbar"
 import "./ProductStyle.css"
 import { Link,useParams} from "react-router-dom"
 import Item from "../products.json"
-import React from "react"
+import {React,useState} from "react"
 const ProductDetail = () =>{
     let {page} = useParams();
+    const [isHover, setIsHover] = useState(false);
     // eslint-disable-next-line
     const firstCond = (Number(page) == 0);
     // eslint-disable-next-line
     const secCond = (Number(page) == 38);
-    
+    const colorStyle = () => ({
+        color: isHover ? Item[page].background_color: `white`,
+        border: isHover ? `2px solid ${Item[page].background_color}` : `2px solid white` 
+    });
+    const handleMouseEnter = () => {
+        setIsHover(true);
+     };
+  
+     const handleMouseLeave = () => {
+        setIsHover(false);
+     };
     return(
             <div className="detail-body">
                 <div className="detail-background" style={{backgroundColor:Item[page].background_color}}></div>
@@ -42,28 +53,57 @@ const ProductDetail = () =>{
                 </>)}
                     
                 <div className="detail-container">
-                    <div className="detail-box">
-                        <div className="inside-box">
-                            <h2>{Item[page].name}</h2>
-                            <h3>NUTRITION FACTS</h3>
-                            <p>Serving Size {Item[page].nutrition_facts.serving_size[0]} ({Item[page].nutrition_facts.serving_size[1]})</p>
-                            <p>Servings Per Container {Item[page].nutrition_facts.servings_per_container[0]}</p>
-                            <p style={{marginTop:"10px", borderTop:"1px solid black"}}>Amount per Serving</p>
-                            <p style={{borderBottom:"5px solid black"}}>Calories {Item[page].nutrition_facts.calories}</p>
-                            <p style={{textAlign:"right"}}>% Daily Value *</p>
-                            <p>Total fat {Item[page].nutrition_facts.daily_value.fat[0]}<span>{Item[page].nutrition_facts.daily_value.fat[1]}</span></p>
-                            <p>Sodium {Item[page].nutrition_facts.daily_value.sodium[0]}<span>{Item[page].nutrition_facts.daily_value.sodium[1]}</span></p>
-                            <p>Total Carbohydrate {Item[page].nutrition_facts.daily_value.carbohydrate[0]}<span>{Item[page].nutrition_facts.daily_value.carbohydrate[1]}</span></p>
-                            <p>Sugars {Item[page].nutrition_facts.daily_value.sugars}</p>
-                            <p style={{borderBottom:"5px solid black"}}>Protein {Item[page].nutrition_facts.daily_value.protein}</p>
+                    <div className="detail-left">
+                        <h1>{Item[page].name}</h1>
+                        <h4>NUTRITION FACTS</h4>
+                        <div style={{display:"flex"}}>
+                            <div className="detail-item-box">
+                                <p>Amount <br/>per Serving</p>
+                                <h2>{Item[page].nutrition_facts.calories} cl</h2>
+                                <div className="detail-item-footer">
+
+                                </div>
+                            </div>
+                            <div className="detail-item-box">
+                                <p>Total Fat</p>
+                                <h2>{Item[page].nutrition_facts.daily_value.fat[0]}</h2>
+                                <div className="detail-item-footer">
+
+                                </div>
+                            </div>
+                            <div className="detail-item-box">
+                                <p>Sodium</p>
+                                <h2>{Item[page].nutrition_facts.daily_value.sodium[0]}</h2>
+                                <div className="detail-item-footer">
+
+                                </div>
+                            </div>
+                            <div className="detail-item-box">
+                                <p>Total Carbohydrate</p> 
+                                <h2>{Item[page].nutrition_facts.daily_value.carbohydrate[0]}</h2>
+                                <div className="detail-item-footer">
+
+                                </div>
+                            </div>
+                            <div className="detail-item-box">
+                                <p>Protein</p> 
+                                <h2>{Item[page].nutrition_facts.daily_value.protein}</h2>
+                                <div className="detail-item-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="detail-left-footer">
                             <p>Not a significant source of other nutrients.</p>
                             <p>*Percent Daily Values are based on a 2,000 calorie diet.</p>
                         </div>
-                        <div className="navbar-right">
-                                <Link to="/purches"><div className='navbar-item'>Buy now</div></Link>
-                        </div>
+                        <Link to="/purches" className="detail-navbar" style={colorStyle()} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                                <div className="detail-nav-item">BUY NOW</div>
+                        </Link>
                     </div>
-                    <img className="detail-img" src={`../${Item[page].img_product}`} alt=""/>
+                    <div className="detail-right">
+                        <img className="detail-img" src={`../${Item[page].img_product}`} alt=""/>
+                    </div>
                 </div>
             </div>
     )
