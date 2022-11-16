7d6285bdfbb04f672d9c932926828ed48881cbb2
Teerawat Sangjun <88505847+TeerawatSangjun@users.noreply.github.com>
2022-11-16 21:26:51 +0700

    navbar login and little navbar animation

--- pepsi/src/components/Logincpn/LoginPage.js
+++ pepsi/src/components/Logincpn/LoginPage.js
@@ -1,10 +1,12 @@
 import '../Logincpn/LoginPage.css';
 import FormPage from './FormPage'
 import InfoPage from './InfoPage';
+import Navbar from "../HomePage/navbar";
 
 const LoginPage = () => {
     return (
         <div className='page-login'>
+            <Navbar></Navbar>
             <div className="card-login">
                 <InfoPage/>
                 <FormPage/>
