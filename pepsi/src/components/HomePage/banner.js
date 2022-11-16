565ec53539c84d3e4b11c1a64ce0f0bd8fe27b2b
Xzep <zephyrxzep@gmail.com>
2022-11-16 16:56:04 +0700

    Nu

--- pepsi/src/components/HomePage/banner.js
+++ pepsi/src/components/HomePage/banner.js
@@ -3,7 +3,19 @@ import logo from '../assets/homePage/logo.png'
 const Banner = ()=>{
     return(
         <div className='banner-container'>
-            <div className='left-container'></div>
+            <div className='left-container'>
+                <div className='top-quote'>KUMI SHOP</div>
+                <div className='mid-quote'>
+                    <div>NEED</div>
+                    <div>SOME</div>
+                    <div>DRINK?</div>
+                </div>
+                <div className='bot-quote'>
+                    <div className='left-quote'>Fast delivery</div>
+                    <div className='center-quote'>Agent prices</div>
+                    <div className='right-quote'>Many products</div>
+                </div>
+            </div>
             <div className='right-container'></div>
         </div>
     )
