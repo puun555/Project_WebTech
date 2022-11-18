983a05e7edd6809f17e102749ef9b2722dbc7fe4
Xzep <zephyrxzep@gmail.com>
2022-11-18 16:08:17 +0700

    Nu edits

--- pepsi/src/components/HomePage/VdoPage.js
+++ pepsi/src/components/HomePage/VdoPage.js
@@ -3,7 +3,7 @@ import './VdoPage.css'
 const VdoPage = () =>{
     return (
         <div className='vdo-container'>
-            <video className='vdo' autoPlay loop controls muted>
+            <video className='vdo' autoPlay preload loop controls>
                 <source src='vdo/vdo.mp4'></source>
             </video>
         </div>
