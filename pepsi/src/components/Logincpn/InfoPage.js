8ad872cfa2e7b1beeb4de625331359ca564586a6
sahadsawatbp <80098729+sahadsawatbp@users.noreply.github.com>
2022-11-20 22:22:04 +0700

    Update InfoPage.js

--- pepsi/src/components/Logincpn/InfoPage.js
+++ pepsi/src/components/Logincpn/InfoPage.js
@@ -4,7 +4,7 @@ const InfoPage = () => {
     return(
         <div className='info-page'>
             <h2>KUMI SHOP</h2>
-            <h1>THE BIGGEST<br/>BEVERAGE<br/>SHOP<br/>IN THILAND</h1>
+            <h1>THE BIGGEST<br/>BEVERAGE<br/>SHOP<br/>IN THAILAND</h1>
         </div>
     );
 }
